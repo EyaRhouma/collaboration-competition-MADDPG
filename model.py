80f8f2fd7da447ba676f02789a2f2d6329e0e2d4
EyaRhouma <rhouma.eya@gmail.com>
2019-10-06 20:40:03 +0200

    Add critic  model using torch

--- model.py
+++ model.py
@@ -49,3 +49,39 @@ class ActorModel(nn.Module):
         x = F.tanh(self.fc3(x))
         return x
 
+
+class CriticModel(nn.Module):
+    """Critic (Value) Model."""
+
+    def __init__(self, input_size, seed, fc1_units=256, fc2_units=256):
+        """Initialize parameters and build model.
+        Params
+        ======
+            input_size (int): number of dimensions for input layer
+            seed (int): random seed
+            fc1_units (int): number of nodes in the first hidden layer
+            fc2_units (int): number of nodes in the second hidden layer
+        """
+        super(CriticModel, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.fc1 = nn.Linear(input_size, fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, 1)
+        self.bn1 = nn.BatchNorm1d(fc1_units)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        """Initialize weights with near zero values."""
+        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
+        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+
+    def forward(self, states, actions):
+        """Build a critic network that maps (states, actions) pairs to Q-values."""
+        xs = torch.cat((states, actions), dim=1)
+        x = F.relu(self.fc1(xs))
+        x = self.bn1(x)
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
